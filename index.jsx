2b8d9cfdcfba1b1ab18a248438882b262030a0e3
picciolialice <pediatricnursealice@gmail.com>
2025-05-01 09:01:33 +0200

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,68 @@
+<div style={{width: 412, height: 2114, position: 'relative', background: '#FFF8F2', overflow: 'hidden'}}>
+  <div style={{width: 118, height: 35, left: 34, top: 439, position: 'absolute', background: '#DF4C39', borderRadius: 23}} />
+  <div style={{left: 57, top: 448, position: 'absolute', color: '#FBE6B7', fontSize: 14, fontFamily: 'Lato', fontWeight: '600', wordWrap: 'break-word'}}>Prenota ora</div>
+  <div style={{width: 463, height: 454, left: 176, top: 76, position: 'absolute', background: '#FBE6B7', borderRadius: 9999}} />
+  <div style={{left: 34, top: 196, position: 'absolute', color: '#DF4C39', fontSize: 35, fontFamily: 'League Spartan', fontWeight: '700', wordWrap: 'break-word'}}>Infermiera Pediatrica</div>
+  <div style={{left: 34, top: 167, position: 'absolute', color: '#67452D', fontSize: 26, fontFamily: 'League Spartan', fontWeight: '700', wordWrap: 'break-word'}}>Dott.ssa Alice Piccioli</div>
+  <div style={{width: 345, left: 34, top: 298, position: 'absolute', color: '#352317', fontSize: 16, fontFamily: 'Lato', fontWeight: '400', wordWrap: 'break-word'}}>Vi accompagno a vivere con fiducia ed entusiasmo le meravigliose tappe di crescita da 0 a 3 anni.</div>
+  <div style={{left: 34, top: 259, position: 'absolute', color: '#352317', fontSize: 20, fontFamily: 'League Spartan', fontWeight: '600', wordWrap: 'break-word'}}>Ogni giorno è una nuova scoperta</div>
+  <div style={{width: 354, left: 34, top: 354, position: 'absolute', color: '#352317', fontSize: 16, fontFamily: 'Lato', fontWeight: '400', wordWrap: 'break-word'}}>Ogni famiglia è unica. Il mio approccio è pensato per accogliervi e fornirvi la guida su misura che meritate.</div>
+  <div style={{width: 345, left: 34, top: 596, position: 'absolute', color: '#352317', fontSize: 16, fontFamily: 'Lato', fontWeight: '500', wordWrap: 'break-word'}}>Scoprite come posso aiutarvi</div>
+  <img style={{width: 32, height: 32, left: 347, top: 72, position: 'absolute'}} src="https://placehold.co/32x32" />
+  <div style={{left: 34, top: 508, position: 'absolute', color: '#DF4C39', fontSize: 35, fontFamily: 'League Spartan', fontWeight: '700', wordWrap: 'break-word'}}>Genitori, <br/>non siete soli! </div>
+  <div style={{width: 305, height: 177, left: 54, top: 656, position: 'absolute', background: '#DF4C39', borderRadius: 18.57}} />
+  <div style={{width: 122, height: 35, left: 69, top: 770, position: 'absolute', background: '#FBE6B7', borderRadius: 23}} />
+  <div style={{left: 118, top: 779, position: 'absolute', color: 'black', fontSize: 14, fontFamily: 'Lato', fontWeight: '400', wordWrap: 'break-word'}}>Info</div>
+  <div style={{width: 304, height: 162, left: 54, top: 1023, position: 'absolute', background: '#DF4C39', borderRadius: 18.57}} />
+  <div style={{width: 100, height: 35, left: 80, top: 1125, position: 'absolute', background: '#FBE6B7', borderRadius: 23}} />
+  <div style={{width: 25, left: 118, top: 1134, position: 'absolute', color: '#352317', fontSize: 14, fontFamily: 'Lato', fontWeight: '400', wordWrap: 'break-word'}}>Info</div>
+  <div style={{width: 132, height: 67, left: 207, top: 684, position: 'absolute', color: '#FFF8F2', fontSize: 16, fontFamily: 'Lato', fontWeight: '400', wordWrap: 'break-word'}}>Hai difficoltà ad allattare? Pensi che il tuo bimbo non cresca abbastanza? Non avere paura, posso aiutarti!</div>
+  <div style={{width: 138, left: 207, top: 1041, position: 'absolute', color: '#FFF8F2', fontSize: 16, fontFamily: 'Lato', fontWeight: '400', wordWrap: 'break-word'}}>Hai bisogno di supporto nei primi giorni insieme al
+  <br/>tuo bimbo? Posso aiutarti seguendoti direttamente
+  <br/>a casa tua</div>
+  <div style={{left: 69, top: 733, position: 'absolute', color: '#FFF8F2', fontSize: 24, fontFamily: 'League Spartan', fontWeight: '500', wordWrap: 'break-word'}}>Allattamento</div>
+  <div style={{left: 80, top: 1091, position: 'absolute', color: '#FFF8F2', fontSize: 24, fontFamily: 'League Spartan', fontWeight: '500', wordWrap: 'break-word'}}>Post parto</div>
+  <div style={{width: 300, height: 50, left: 34, top: 1440, position: 'absolute', color: '#DF4C39', fontSize: 35, fontFamily: 'League Spartan', fontWeight: '600', wordWrap: 'break-word'}}>Perché scegliermi?</div>
+  <div style={{width: 322, left: 50, top: 1533, position: 'absolute', color: '#352317', fontSize: 16, fontFamily: 'Lato', fontWeight: '500', wordWrap: 'break-word'}}>Lavoro tutti i giorni per creare una generazione di genitori  consapevoli e sicuri nel percorso di crescita dei loro bambini.</div>
+  <div style={{width: 322, left: 50, top: 1643, position: 'absolute', color: '#352317', fontSize: 16, fontFamily: 'Lato', fontWeight: '500', wordWrap: 'break-word'}}>Attraverso consulenze mirate e personalizzate, supporto emotivo e formazione pratica, aiuto le famiglie a navigare con serenità le fasi cruciali dello sviluppo infantile, lavorando in sinergia con la rete pediatrica locale.</div>
+  <div style={{width: 330, left: 50, top: 1786, position: 'absolute', color: '#352317', fontSize: 16, fontFamily: 'Lato', fontWeight: '500', wordWrap: 'break-word'}}>Non lavoro da sola: sono sempre pronta a collaborare con la rete pediatrica e altri professionisti della salute</div>
+  <div style={{width: 33.87, height: 34.82, left: 113, top: 1048, position: 'absolute', background: '#FFF8F2'}} />
+  <div style={{width: 254, height: 0, left: 34.25, top: 1796, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: 'top left', outline: '1px #FBE6B7 solid', outlineOffset: '-0.50px'}}></div>
+  <div style={{width: 24, height: 24, left: 22, top: 1530, position: 'absolute'}}>
+    <div style={{width: 9, height: 9, left: 7.50, top: 7.50, position: 'absolute', background: '#DF4C39', outline: '2px #DF4C39 solid', outlineOffset: '-1px'}} />
+  </div>
+  <div style={{width: 24, height: 24, left: 22, top: 1639, position: 'absolute'}}>
+    <div style={{width: 9, height: 9, left: 7.50, top: 7.50, position: 'absolute', background: '#DF4C39', outline: '2px #DF4C39 solid', outlineOffset: '-1px'}} />
+  </div>
+  <div style={{width: 24, height: 24, left: 22, top: 1784, position: 'absolute'}}>
+    <div style={{width: 9, height: 9, left: 7.50, top: 7.50, position: 'absolute', background: '#DF4C39', outline: '2px #DF4C39 solid', outlineOffset: '-1px'}} />
+  </div>
+  <div style={{width: 36, height: 36, left: 113, top: 686, position: 'absolute'}}>
+    <div style={{width: 13.62, height: 13.62, left: 19.50, top: 2.88, position: 'absolute', outline: '2px #FFF8F2 solid', outlineOffset: '-1px'}} />
+    <div style={{width: 16.76, height: 16.76, left: 14.12, top: 5.12, position: 'absolute', outline: '2px #FFF8F2 solid', outlineOffset: '-1px'}} />
+    <div style={{width: 22.46, height: 22.46, left: 3.04, top: 10.50, position: 'absolute', outline: '2px #FFF8F2 solid', outlineOffset: '-1px'}} />
+  </div>
+  <div style={{width: 305, height: 170, left: 54, top: 843, position: 'absolute', background: '#DF4C39', borderRadius: 18.57}} />
+  <div style={{width: 130, height: 35, left: 64, top: 946, position: 'absolute', background: '#FBE6B7', borderRadius: 23}} />
+  <div style={{width: 27, left: 116, top: 955, position: 'absolute', color: '#352317', fontSize: 14, fontFamily: 'Lato', fontWeight: '400', wordWrap: 'break-word'}}>Info</div>
+  <div style={{width: 121, height: 127, left: 209, top: 860, position: 'absolute', color: '#FFF8F2', fontSize: 16, fontFamily: 'Lato', fontWeight: '400', wordWrap: 'break-word'}}>Il tuo bimbo deve iniziare a introdurre gli alimenti solidi ma non ti senti sicura? Sono qui per questo!</div>
+  <div style={{left: 64, top: 906, position: 'absolute', color: '#FFF8F2', fontSize: 24, fontFamily: 'League Spartan', fontWeight: '500', wordWrap: 'break-word'}}>Svezzamento</div>
+  <div style={{width: 33, height: 33, left: 113, top: 862, position: 'absolute', overflow: 'hidden'}}>
+    <div style={{width: 27.84, height: 28.88, left: 2.06, top: 2.06, position: 'absolute', background: '#FFF8F2'}} />
+  </div>
+  <div style={{width: 304, height: 182, left: 54, top: 1196, position: 'absolute', background: '#DF4C39', borderRadius: 18.57}} />
+  <div style={{width: 94, height: 35, left: 82, top: 1312, position: 'absolute', background: '#FBE6B7', borderRadius: 23}} />
+  <div style={{width: 25, left: 117, top: 1321, position: 'absolute', color: '#352317', fontSize: 14, fontFamily: 'Lato', fontWeight: '400', wordWrap: 'break-word'}}>Info</div>
+  <div style={{width: 127, left: 207, top: 1219, position: 'absolute', color: '#FFF8F2', fontSize: 16, fontFamily: 'Lato', fontWeight: '400', wordWrap: 'break-word'}}>Il tuo bimbo ha dermatite da pannolino o la ha
+  <br/>frequentemente? Per queste e altre problematiche posso assistervi</div>
+  <div style={{left: 81, top: 1257, position: 'absolute', textAlign: 'center', color: '#FFF8F2', fontSize: 24, fontFamily: 'League Spartan', fontWeight: '500', wordWrap: 'break-word'}}>Cura<br/>della cute</div>
+  <div style={{width: 31, height: 34.82, left: 114, top: 1211, position: 'absolute', background: '#FFF8F2'}} />
+  <div style={{width: 412, height: 163, left: 0, top: 1951, position: 'absolute', background: '#FBE6B7'}} />
+  <div style={{left: 33, top: 1969, position: 'absolute', color: 'black', fontSize: 16, fontFamily: 'Lato', fontWeight: '400', lineHeight: 20, wordWrap: 'break-word'}}>Termini e condizioni<br/>Cookie policy<br/>Informativa sulla privacy<br/>P.IVA      02994230999<br/><br/>©2025 infermieradeibimbi.com</div>
+  <div style={{width: 17.10, height: 17.10, left: 334.95, top: 2010, position: 'absolute', background: 'black'}} />
+  <div style={{width: 17, height: 17, left: 303, top: 2010.05, position: 'absolute', outline: '1.50px black solid', outlineOffset: '-0.75px'}} />
+  <div style={{width: 8.95, height: 8.95, left: 307.47, top: 2013.63, position: 'absolute', outline: '1.50px black solid', outlineOffset: '-0.75px'}} />
+  <div style={{width: 17.10, height: 17.10, left: 334.95, top: 2010, position: 'absolute', background: 'black'}} />
+  <div style={{width: 17, height: 17, left: 303, top: 2010.05, position: 'absolute', outline: '1.50px black solid', outlineOffset: '-0.75px'}} />
+  <div style={{width: 8.95, height: 8.95, left: 307.47, top: 2013.63, position: 'absolute', outline: '1.50px black solid', outlineOffset: '-0.75px'}} />
+</div>
